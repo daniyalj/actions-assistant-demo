a08ea7c474274d91085eeef493f76dd7ecb78f55
Daniyal <daniyaljavedk@gmail.com>
2019-11-28 10:34:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,23 @@ terraform {
     prefix = "terraform/state"
   }
 }
+
+resource "random_id" "instance_id" {
+ byte_length = 8
+}
+
+resource "google_compute_instance" "default" {
+ name         = "assistant-instance-${random_id.instance_id.hex}"
+ machine_type = "f1-micro"
+ zone         = "northamerica-northeast1"
+
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
+
+ network_interface {
+   network = "default"
+ }
+}
