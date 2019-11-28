de44ed7fbf079a15dac05cfde122640d8b3a0d79
Daniyal <daniyaljavedk@gmail.com>
2019-11-28 10:50:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "random_id" "instance_id" {
 resource "google_compute_instance" "default" {
   name         = "assistant-instance-${random_id.instance_id.hex}"
   machine_type = "f1-micro"
-  zone         = "northamerica-northeast1"
+  zone         = "us-east1-b"
 
   boot_disk {
     initialize_params {
